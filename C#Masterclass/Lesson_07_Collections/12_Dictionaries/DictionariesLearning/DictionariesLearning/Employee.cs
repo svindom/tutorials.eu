7de112df90930929786f89b19f7ae117a6dc1d54
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-12-15 23:01:33 +0200

    Learning the Dictionaries in C#

---
+++ C#Masterclass/Lesson_07_Collections/12_Dictionaries/DictionariesLearning/DictionariesLearning/Employee.cs
@@ -0,0 +1,26 @@
+﻿class Employee
+{
+    // Properties
+    public string Role { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public float Rate { get; set; }
+
+    // Yearly Salary = rate/h * number of days * number of weeks * number of month
+    public float Salary
+    {
+        get
+        {
+            return Rate * 8 * 5 * 4 * 12;
+        }
+    }
+
+    public Employee(string role, string name, int age, float rate) 
+    {
+        this.Role = role;
+        this.Name = name;
+        this.Age = age;
+        this.Rate = rate;
+
+    }
+}
--- C#Masterclass/Lesson_07_Collections/12_Dictionaries/DictionariesLearning/DictionariesLearning/Program.cs
+++ C#Masterclass/Lesson_07_Collections/12_Dictionaries/DictionariesLearning/DictionariesLearning/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+Employee[] emploees = { 
+    new Employee("CEO", "Gwyn", 45, 200),
+    new Employee("Manager","Joe", 35, 150),
+    new Employee("HR", "Lora", 24, 95),
+    new Employee("Secretary", "Petra", 27, 90),
+    new Employee("Lead Developer", "Arthur", 58, 180),
+    new Employee("Intern", "Ernest", 22, 20)
+};
+
+Dictionary<string, Employee> employeesDirectory = new Dictionary<string, Employee>();
+foreach (Employee emploee in emploees)
+{
+    employeesDirectory.Add(emploee.Role, emploee);
+}
+
+string key = "CEO";
+if (employeesDirectory.ContainsKey(key))
+{
+    Employee employeeInfo = employeesDirectory[key];
+    Console.WriteLine($"{employeeInfo.Name}, {employeeInfo.Role}, {employeeInfo.Salary}$");
+}
+else
+{
+    Console.WriteLine($"No emploee founf with this key {key}");
+}
+
+Console.ReadKey();
+
+
+
+
