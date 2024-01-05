a37525c7b0648870c5563fc395a5d22e251a6409
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-01-05 23:13:55 +0200

    revising the inheritance in C#

--- C#Masterclass/Lesson_08_Inheritance/InheritanceLearning/InheritanceLearning/Program.cs
+++ C#Masterclass/Lesson_08_Inheritance/InheritanceLearning/InheritanceLearning/Program.cs
@@ -1,1 +1,87 @@
-﻿
+﻿Radio radio = new Radio(false, "BBC");
+radio.Brand = "BBC4";
+
+// we are using the method from the base class
+radio.SwitchOn();
+// we are using the method from the child class
+radio.ListenRadio();
+
+
+
+// the same can be done with the TV
+
+Console.ReadKey();
+
+
+
+
+
+// Parent - Base class
+public class ElectricalDevice
+{
+    // boolean to determine the state of the device
+    public bool IsOn { get; set; }
+    // string for the brand name of the device
+    public string Brand { get; set; }
+
+    public ElectricalDevice(bool isOn, string brand)
+    {
+        IsOn = isOn;
+        Brand = brand;
+    }
+
+    // switch on the device
+    public void SwitchOn()
+    {
+        IsOn = true;
+    }
+    // switch off the device
+    public void SwitchOff()
+    {
+        IsOn = false;
+    }
+}
+
+// Child Class (inherited from the ElectricalDevice class)
+public class Radio : ElectricalDevice
+{
+    public Radio(bool isOn, string brand):base(isOn,brand)
+    {
+    }
+
+
+    public void ListenRadio()
+    {
+        if (IsOn) 
+        {
+            Console.WriteLine($"Listening to the {Brand} radio");
+        }
+        else
+        {
+            Console.WriteLine("Radio is switched off, you need to switch it on first");
+        }
+    }
+}
+
+
+// Child Class (inherited from the ElectricalDevice class)
+public class TV : ElectricalDevice
+{
+    public TV(bool isOn, string brand): base(isOn, brand)
+    {
+    
+    }
+    public void WatchingTV()
+    {
+        if (IsOn)
+        {
+            Console.WriteLine("Listening to the TV");
+        }
+        else
+        {
+            Console.WriteLine("TV is switched off, you need to switch it on first");
+        }
+    }
+}
+
+
---
+++ C#Masterclass/Lesson_08_Inheritance/VirtualOverrideDemo/VirtualOverrideDemo/Program.cs
@@ -0,0 +1,97 @@
+﻿Dog myDog = new Dog("Charly", 8);
+Console.WriteLine($"{myDog.Name} is {myDog.Age} years old");
+myDog.Play();
+myDog.Eat();
+myDog.MakeSound();
+
+
+
+
+Console.ReadKey();
+
+
+
+
+
+// Base class
+public class Animal
+{
+    // properties
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public bool IsHungry { get; set; }
+
+    // constructor
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+        // in our case all animals are hungry by default
+        IsHungry = true;
+    }
+
+    // an empty virtual method MakeSound for other classes to override
+    public virtual void MakeSound()
+    {
+        
+    }
+
+    // a virtual method which sub classes (children or derived) can override
+    public virtual void Eat()
+    {
+        if (IsHungry)
+        {
+            Console.WriteLine($"The {Name} is eating");
+        }
+        else
+        {
+            Console.WriteLine($"The {Name} is happy and doesn't want to eat");
+        }
+    }
+
+    public virtual void Play()
+    {
+        Console.WriteLine($"The {Name} is plaing");
+    }
+}
+
+// Child class
+class Dog : Animal
+{
+    // bool property to check if the dog is happy
+    public bool IsHappy { get; set; }
+
+    // as we are inheriating from the Animal class we must to create a simple constructor 
+    // where we pass the name and age to our base constructor, specifically Animal.
+    public Dog( string name, int age) : base( name, age ) 
+    {
+        IsHappy = true;
+    }
+
+    // simple override (just reuse the same logic) of the virtual method Eat from the base class Animal
+    public override void Eat()
+    {
+        // to call the Eat method from the base class we use keyword "base".
+        base.Eat();
+    }
+
+    // override of the virtual method MakeSound
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bau bau bau");
+    }
+
+    // override of the virtual method Play
+    public override void Play()
+    {
+        // check if the dog is happy if yes call the Play method from the base class Animal
+        if(IsHappy)
+        {
+            base.Play();
+        }
+        else
+        {
+            Console.WriteLine("Make your pet be happy!");
+        }
+    }
+}
