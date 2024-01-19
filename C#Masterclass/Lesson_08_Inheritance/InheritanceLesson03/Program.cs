46af896054530e75e411ab93ad5da8125f819aa1
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-01-19 22:24:19 +0200

    Continuing to learn about inheritance in C#

---
+++ C#Masterclass/Lesson_08_Inheritance/InheritanceLesson03/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Console.ReadKey();
+
+
+public class Post
+{
+    private static int currentPostID;
+
+
+
+    //properties
+    protected int ID { get; set; }
+    protected string Title { get; set; }
+    protected string SendByUserName { get; set; }
+    protected bool IsPublic { get; set;}
+    
+    // Defaulr constructor. If a derived class does not invoke a base class constructor explicitly, 
+    // the default constructor is called implicitly
+    public Post(int iD, string title, string sendByUserName, bool isPublic)
+    {
+        ID = iD;
+        Title = title;
+        SendByUserName = sendByUserName;
+        IsPublic = isPublic;
+    }
+}
