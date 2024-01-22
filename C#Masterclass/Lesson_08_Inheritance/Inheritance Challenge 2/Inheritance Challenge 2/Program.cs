5265e9dc982196cb1214eadd1494c95248fcf1da
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-01-22 22:34:22 +0200

    Finished exercise with Inheritance

--- C#Masterclass/Lesson_08_Inheritance/Inheritance Challenge 2/Inheritance Challenge 2/Program.cs
+++ C#Masterclass/Lesson_08_Inheritance/Inheritance Challenge 2/Inheritance Challenge 2/Program.cs
@@ -1,12 +1,37 @@
-﻿
+﻿using System;
+
+
+int employeeSalary = 3000;
+int bossSalary = 8000;
+int traineeSalary = 1200;
+int traineeWorkingHours = 4;
+int traineeStudingHours = 4;
+
+
+Employee jacky = new Employee("UI designer", "Jacky", employeeSalary);
+Boss harris = new Boss("CEO", "Harris", bossSalary, "BMW");
+Trainee yutta = new Trainee("Programmer trainee", "Yutta", traineeSalary, traineeWorkingHours, traineeStudingHours);
+
+jacky.Work();
+jacky.Pause();
+Console.WriteLine("----------------------------------------");
+harris.Lead();
+harris.Work();
+harris.Pause();
+Console.WriteLine("----------------------------------------");
+yutta.Learn();
+yutta.Work();
+yutta.Pause();
+
+
 
 Console.ReadKey();
 
 public class Employee
 {
-    public string Name { get; set; }
-    public string FirstName { get; set; }
-    public int Salary { get; set; }
+    public string Position { get; }
+    public string FirstName { get; }
+    public int Salary { get; }
 
     // Default constructor
     /*
@@ -18,20 +43,20 @@ public class Employee
     }
     */
 
-    public Employee(string name, string firstName, int salary)
+    public Employee(string position, string firstName, int salary)
     {
-        this.Name = name;
+        this.Position = position;
         this.FirstName = firstName;
         this.Salary = salary;
     }
 
     public virtual void Work()
     {
-        
+        Console.WriteLine($"{FirstName} is saying \"I'm working\"");
     }
     public void Pause()
     {
-
+        Console.WriteLine($"{FirstName} is saying \"I'm having a break\"");
     }
 }
 
@@ -39,18 +64,14 @@ public class Boss : Employee
 {
     public string CompanyCar { get; set; }
 
-    public Boss(string name, string firstName, int salary, string companyCar) : base(name, firstName, salary)
+    public Boss(string position, string firstName, int salary, string companyCar) : base (position, firstName, salary) // : base - gives us the possibility of not rewriting the constructor from the main class
     {
-        this.Name = name;
-        this.FirstName = firstName;
-        this.Salary = salary;
-
         this.CompanyCar = companyCar;
     }
 
     public void Lead()
     {
-
+        Console.WriteLine($"{FirstName} is saying \"I'm a CEO of the company\"");
     }
 }
 
@@ -59,18 +80,18 @@ public class Trainee : Employee
     public int WorkingHours { get; set; }
     public int SchoolHours { get; set; }
 
-    public Trainee (string name, string firstName, int salary, int workingHours, int schoolHours) : base (name, firstName, salary)
+    public Trainee (string position, string firstName, int salary, int workingHours, int schoolHours) : base (position, firstName, salary) // : base - gives us the possibility of not rewriting the constructor from the main class
     {
-        this.Name = name;
-        this.FirstName = firstName;
-        this.Salary = salary;
-
         this.WorkingHours = workingHours;
         this.SchoolHours = schoolHours;
     }
     
     public void Learn()
     {
-
+        Console.WriteLine($"{FirstName} is saying \"I'm a trainee here and I'm studing for {SchoolHours} hours at school\"");
+    }
+    public override void Work()
+    {
+        Console.WriteLine($"{FirstName} is saying \"I'm a trainee here and I'm working for {WorkingHours} hours at the company.\"");
     }
 }
