83339d8a242e4044a96811228d1cef39476c042b
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-01-23 23:23:47 +0200

    Working on Interfaces

---
+++ C#Masterclass/Lesson_08_Inheritance/01_Interfaces_Basics/Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿Ticket ticket1 = new Ticket(10);
+Ticket ticket2 = new Ticket(10);
+
+Console.WriteLine(ticket1.Equals(ticket2));
+
+Console.ReadLine();
+
+public class Ticket : IEquatable<Ticket>
+{
+    // property to store the duration of the ticket in hours
+
+    public int DurationInHours { get; set; }
+
+    // simple constructor
+
+    public Ticket(int durationInHours)
+    {
+        DurationInHours = durationInHours;
+    }
+
+    public bool Equals(Ticket ticket)
+    {
+        // we compare to instances aand if they both have the same DurationInHours it will return true
+        return this.DurationInHours == ticket.DurationInHours;
+    }
+}
---
+++ C#Masterclass/Lesson_08_Inheritance/Interfaces_2/Interfaces_2/Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+Table table = new Table("white", "wood");
+
+
+Console.WriteLine($"This is my {table.Color} table and it has {table.Material} material");
+Console.ReadKey();
+
+
+
+
+class Furniture
+{
+    public string Color { get; set;}
+    public string Material { get; set; }
+
+    
+    public Furniture(string color, string material)
+    {
+        Color = color;
+        Material = material;
+    }
+
+    public Furniture() 
+    {
+        Color = "White";
+        Material = "Wood";
+    }
+    
+}
+
+
+class Chair : Furniture
+{
+    
+    public Chair(string color, string material) :base (color, material)
+    {
+
+    }
+    
+}
+
+class Table : Furniture
+{
+    
+    public Table (string color, string material) : base (color, material)
+    {
+
+    }
+    
+}
+
+class Vehicle
+{
+    public float Speed { get; set; }
+    public string Color { get; set; }
+
+    public Vehicle(float speed, string color)
+    {
+        Speed = speed;
+        Color = color;
+    }
+
+    public Vehicle() 
+    {
+        Speed = 120f;
+        Color = "Black";
+    }
+}
+
+class Car : Vehicle
+{
+    public Car(float speed, string color) :base (speed, color) 
+    {
+
+    }
+}
