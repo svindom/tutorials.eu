3a6d6ee5afb54ab12d682b22e055532a72bd49eb
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-02-02 21:49:41 +0200

    Worked with the 'Math' class

---
+++ C#Masterclass/Lesson_09_AdvansedCSharp/MathDemo/MathDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MathDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numToManip = 16.4f;
+
+            Console.WriteLine($"Ceiling: {Math.Ceiling(numToManip)}");
+            Console.WriteLine();
+            Console.WriteLine($"Floor: {Math.Floor(numToManip)}");
+
+            // Power of num
+            int num1 = 6;
+            int num2 = 7;
+            double resultPowerOf = Math.Pow(num1, num2);
+
+            Console.WriteLine($"{num1} to the power of {num2} is {resultPowerOf}");
+            Console.WriteLine();
+
+            // Calculate PI
+            double numberPI = Math.PI;
+            Console.WriteLine($"The number PI is: {numberPI}");
+            Console.WriteLine();
+
+            // Clculate the squre root
+            int num3 = 25;
+            double squareRootNum = Math.Sqrt(num3);
+            Console.WriteLine($"The square root of {num3} is: {squareRootNum}");
+            Console.WriteLine();
+
+            // Calulate cos
+            int num4 = 1;
+            double cosNum = Math.Cos(num4);
+            Console.WriteLine($"The cosof {num3} is: {cosNum}");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
