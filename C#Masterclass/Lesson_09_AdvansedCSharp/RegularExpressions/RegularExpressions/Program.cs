b39cbb75070c57a9ad791dd9917313fed87224fd
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-02-04 22:04:44 +0200

    was learning General Expression

---
+++ C#Masterclass/Lesson_09_AdvansedCSharp/RegularExpressions/RegularExpressions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\d";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi there, my number is 123456789";
+
+            MatchCollection matchCollection = regex.Matches(text);
+
+            Console.WriteLine($"{matchCollection.Count} hits found:\n{text}");
+
+            foreach (Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine($"{group[0].Value} found at {group[0].Index}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
