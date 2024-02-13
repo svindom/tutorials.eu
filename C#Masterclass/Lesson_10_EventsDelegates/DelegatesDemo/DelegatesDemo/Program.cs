791103bbda1251c757f49d5b1a2b61364cb73c3f
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-02-13 22:47:03 +0200

    Add C# code as a part of Delegates learning

---
+++ C#Masterclass/Lesson_10_EventsDelegates/DelegatesDemo/DelegatesDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DelegatesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Alex"};
+
+            Console.WriteLine("-----------before-----------");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.RemoveAll(Filter);
+
+            Console.WriteLine("-----------after-----------");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine();
+        }
+
+        // method that takes a string
+        static bool Filter (string s)
+        {
+            // Return whether the string s contains the letter 'i'
+            // the Contains method will return a bool which we will return as well
+            return s.Contains("i");
+        }
+
+
+    }
+}
---
+++ C#Masterclass/Lesson_10_EventsDelegates/DelegatesDemo/DelegatesDemo2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DelegatesDemo2
+{
+    internal class Program
+    {
+        // defining a delegate type called FilterDelegate that takes a person object and return a bool
+        public delegate bool FilterDelegate(Person person);
+
+        static void Main(string[] args)
+        {
+            Person person1 = new Person()
+            {
+                Name = "Katty",
+                Age = 21,
+            };
+            Person person2 = new Person() { Name = "Lora", Age = 22 };
+            Person person3 = new Person() { Name = "Karina", Age = 28 };
+            Person person4 = new Person() { Name = "Kristina", Age = 33 };
+
+            List<Person> list = new List<Person>() { person1, person2, person3, person4 };
+
+
+
+            Console.ReadLine();
+        }
+        static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        {
+
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
