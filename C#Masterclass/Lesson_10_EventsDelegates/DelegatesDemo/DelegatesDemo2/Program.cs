dbd6ab5baa78a3dd7361b733cf29603ec1e38a89
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-02-23 23:34:08 +0200

    Add delegate learning part

--- C#Masterclass/Lesson_10_EventsDelegates/DelegatesDemo/DelegatesDemo2/Program.cs
+++ C#Masterclass/Lesson_10_EventsDelegates/DelegatesDemo/DelegatesDemo2/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
+﻿
 namespace DelegatesDemo2
 {
     internal class Program
@@ -9,6 +8,7 @@ namespace DelegatesDemo2
 
         static void Main(string[] args)
         {
+            // Creating 4 objects of the class "Person"
             Person person1 = new Person()
             {
                 Name = "Katty",
@@ -17,16 +17,60 @@ namespace DelegatesDemo2
             Person person2 = new Person() { Name = "Lora", Age = 22 };
             Person person3 = new Person() { Name = "Karina", Age = 28 };
             Person person4 = new Person() { Name = "Kristina", Age = 33 };
+            Person person5 = new Person() { Name = "Jhoanna", Age = 73 };
 
-            List<Person> list = new List<Person>() { person1, person2, person3, person4 };
+            // Adding them to the List
+            List<Person> people = new List<Person>() { person1, person2, person3, person4, person5 };
 
-
+            DisplayPeople("Kids", people, IsMinor);
+            DisplayPeople("Adults", people, IsAdult);
+            DisplayPeople("Seniors", people, IsSenior);
 
             Console.ReadLine();
         }
+
+        // a method to display the list of people that passes the filter condition (returns true)
+        // this method will take a title to be displayed, the list of people. and a filter delegate
         static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
         {
+            // print the title
+            Console.WriteLine($"Group of {title}:");
+            Console.WriteLine("---------------------------------------------------");
 
+            bool found = false; // this flag will indicate if any person meets the filter criteria
+
+            foreach (Person person in people)
+            {
+                // check if this person passes the filter
+                if (filter(person))
+                {
+                    Console.WriteLine($"{person.Name} is {person.Age} years old");
+                    found = true;
+                }
+            }
+            
+            if (!found) // Check the flag after the loop
+            {
+                Console.WriteLine("There is no one with that age");
+            }
+            Console.WriteLine();
+        }
+
+        //================ FILTERS ================
+
+        static bool IsMinor (Person person)
+        {
+            return person.Age < 18;
+        }
+
+        static bool IsAdult(Person person)
+        {
+            return person.Age >= 18;
+        }
+
+        static bool IsSenior (Person person)
+        {
+            return person.Age >= 65;
         }
     }
 
---
+++ C#Masterclass/Lesson_10_EventsDelegates/DelegatesDemo/DelegatesPromoMosh/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace DelegatesPromoMosh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoProcessor processor = new PhotoProcessor();
+            PhotoFilters filters = new PhotoFilters();
+
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("photo.jpg", filterHandler);
+
+
+
+
+            Console.ReadKey();
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply Remove Red Eye");
+        }
+    }
+
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+        public void Save()
+        {
+
+        }
+    }
+
+    public class PhotoFilters
+    {
+
+
+        public void ApplyBrightness (Photo photo)
+        {
+            Console.WriteLine("Apply brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+        }
+    }
+
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+
+        public void Process(string path, PhotoFilterHandler photoFilterHandler)
+        {
+            
+
+            Photo photo = Photo.Load(path);
+            photoFilterHandler(photo);
+
+
+            photo.Save();
+        }
+    }
+}
